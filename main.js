c8f7aa6845ac1d2acb90e8b97e937411e5b3e255
HPR98 <henry@hpr-lighting.com>
2024-02-23 02:07:42 +0100

    Finished style started JS

--- main.js
+++ main.js
@@ -1,7 +1,41 @@
+//Change navbar Icon
 const navbar = document.getElementById("navbar")
-
-navbar.addEventListener("click", openNavbar)
-
-function navbar(){
-    document.getElementById("about-me").innerHTML = "About you"
+const navbarClose = document.getElementById("navbar-close")
+function changeIconToClose(){
+    navbar.style.display = "none"
+    navbarClose.style.display =  "inline"
 }
+function changeIconToOpen(){
+    navbar.style.display = "inline"
+    navbarClose.style.display = "none"
+}
+
+
+//Open Navbar
+function showNav(){
+    document.getElementById("nav").style.display = "inline"
+}
+function hideNav(){
+    document.getElementById("nav").style.display = "none"
+}
+
+//Event Listener
+navbar.addEventListener("click", () => {changeIconToClose(); showNav();})
+navbarClose.addEventListener("click", () => {changeIconToOpen(); hideNav();})
+
+
+//Skill reveal icon names
+function revealName(){
+    document.getElementById("HTML-icon").style.top = 0;
+    document.getElementById("HTML-text").style.opacity = 1;
+    document.getElementById("HTML-icon").style.transition = "1s"
+    document.getElementById("HTML-text").style.transition = "1s"
+}
+function hideName(){
+    document.getElementById("HTML-icon").style.top = "30px"
+    document.getElementById("HTML-text").style.opacity= 0;
+}
+
+//Add event listener
+document.getElementById("HTML").addEventListener("mouseover", revealName)
+document.getElementById("HTML").addEventListener("mouseleave", hideName)
